3af023616373287dedc15d2dbfdbc642decaa486
kauanuness <kaua.souza@sptech.school>
2024-07-03 15:01:18 -0300

    criando os metódos do campo

--- campo-minado/src/main/java/br/com/kaua/modelo/Campo.java
+++ campo-minado/src/main/java/br/com/kaua/modelo/Campo.java
@@ -1,5 +1,7 @@
 package br.com.kaua.modelo;
 
+import br.com.kaua.excecao.ExplosaoException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,7 +22,50 @@ public class Campo {
     }
 
     boolean adicionarVizinho (Campo vizinho){
+        boolean linhaDiferente = linha != vizinho.linha;
+        boolean colunaDiferente = coluna != vizinho.coluna;
+        boolean diagonal = linhaDiferente && colunaDiferente;
 
-        return false;
+        int deltaLinha = Math.abs(linha - vizinho.linha);
+        int deltaColuna = Math.abs(coluna - vizinho.coluna);
+        int deltaGeral = deltaColuna + deltaColuna;
+
+        if (deltaGeral == 1 && !diagonal){
+            vizinhos.add(vizinho);
+            return true;
+        } else if (deltaGeral == 2 && diagonal) {
+            vizinhos.add(vizinho);
+            return true;
+        } else{
+            return false;
+        }
     }
+
+    void alterarMarcacao(){
+        if (!aberto) {
+            marcado = !marcado;
+        }
+    }
+
+    boolean abrir(){
+        if (!aberto && !marcado){
+            aberto = true;
+
+            if (minado){
+                throw new ExplosaoException();
+            }
+
+            if (vizinhancaSeguranca()){
+                vizinhos.forEach(v -> v.abrir());
+            }
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    boolean vizinhancaSeguranca(){
+        return vizinhos.stream().noneMatch(v -> v.minado);
+    }
+
 }
